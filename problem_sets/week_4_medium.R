5313ec2d83d4425d29ceff87fc5f3ed78ecb00ed
Sabrina McNew <sabrinamcnew@Sabrina-McNews-MacBook-Pro.local>
2024-08-03 17:26:41 -0700

    Added week 4 exercises

---
+++ problem_sets/week_4_medium.R
@@ -0,0 +1,23 @@
+# ECOL 596 W
+# Week 2 Exercises
+# Levels: Medium-Spicy
+
+
+# Medium ------------------------------------------------------------------
+
+# Let's start by using the diamonds dataset in R. This lists information
+# on nearly 60,000 diamonds More info here: https://rpubs.com/Davo2812/1102821
+
+diamonds <- diamonds
+head(diamonds)
+
+# Create a plot showing the relationship between carat (x) and price (y)
+
+
+# Spicy -------------------------------------------------------------------
+
+# Are you a plotting master?
+# Challenge yourself with a Tidy Tuesday Challenge
+# https://github.com/rfordatascience/tidytuesday
+# more examples/reading here
+# https://towardsdatascience.com/explaining-my-favourite-tidytuesday-projects-e44bfe988813
---
+++ problem_sets/week_4_mild.R
@@ -0,0 +1,5 @@
+# ECOL 596 W
+# Week 2 Exercises
+# Level: Mild
+# Work through Irizarry Ch. 8; do exercises in 8.16
+# https://rafalab.dfci.harvard.edu/dsbook-part-1/dataviz/ggplot2.html
