c34f1b63ad8095adfcb901cd506f638e6d6ab304
Allyson Placko <allyson.placko@gmail.com>
2024-09-12 10:22:36 -0700

    I have thoughts

---
+++ AP_fork_suggestion/my_thoughts.R
@@ -0,0 +1,3 @@
+#I am frightened of the upcomming quiz, even though I know that I do not get grades as
+#I am not actually a student or enrolled in this class. I have so much brain fog from
+#whatever illness I have going on and it makes me dizzy, tough circumstances these days.
