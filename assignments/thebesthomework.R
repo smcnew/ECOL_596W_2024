dff6e739615ecff20b7483191ff56639cf636176
thebatlander <alander@arizona.edu>
2024-09-12 10:10:22 -0700

    Update thebesthomework.R
    Adding a monologue

---
+++ assignments/thebesthomework.R
@@ -0,0 +1,9 @@
+#Where did everyone go?
+#It seems as though the last of the Goo balls were sucked up into a pipe...
+#To the World of Goo Corporation
+#...possibly forever!
+#...So what do we do now?
+#It's probably best to just leave now.
+#Go outside...
+#Forget you ever played this game
+#With love, The Sign Painter
