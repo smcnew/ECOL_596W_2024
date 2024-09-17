5e0bd518a2657bd2451cb44583c1f861cfbc494e
Allyson Placko <allyson.placko@gmail.com>
2024-09-16 18:07:44 -0700

    This is the code to look at the mass of some rare galopagos birds. I would say this is a bad figure since I did basically no formating, so it is missing a title and labels on the sides, and the sample sizes of some of the birds is quite small. :)

--- AP_fork_suggestion/my_thoughts.R
+++ AP_fork_suggestion/my_thoughts.R
@@ -1,3 +1,15 @@
-#I am frightened of the upcomming quiz, even though I know that I do not get grades as
-#I am not actually a student or enrolled in this class. I have so much brain fog from
-#whatever illness I have going on and it makes me dizzy, tough circumstances these days.
+# Plot for Homework
+
+
+library(readr)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+Small_sample_size <- read_csv("AP_fork_suggestion/Small_sample_size.csv")
+
+Small_sample_size%>%
+  filter(!is.na(scale)) %>%
+  ggplot(aes(x = Species, y = Mass)) +
+  geom_boxplot()
+
+#fin
