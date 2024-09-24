624b9498b0d43624c2ea9f0589c578f6da1fc7dd
YinpengZ <yinpengz@arizona.edu>
2024-09-23 19:19:55 -0700

    commit

--- students/Yinpeng.R
+++ students/Yinpeng.R
@@ -1,2 +1,7 @@
 #clone branch of class
 library(usethis)
+usethis::create_from_github(
+  "https://github.com/smcnew/ECOL_596W_2024.git",
+  destdir = "C:/Users/gekko/Desktop/R-prac",
+  fork = TRUE
+)
